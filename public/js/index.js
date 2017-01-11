f52f2256bc31f8d68e1d0e845a7b4f53c7be7e4e
User <santhosh.k101@wipro.com>
2017-01-11 14:03:43 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect',function (){
+  console.log('connected to the server');
+    socket.emit('createMessage',{
+    from : 'kannan',
+    text : 'Hi..! how are you ?'
+  });
+});
+
+socket.on('disconnect',function (){
+  console.log('Disconnected from the server');
+});
+
+
+socket.on('newMessage', function(message){
+  console.log('New message',message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
   console.log('New user connected');
+
+  socket.emit('newMessage',{
+    from:'SANTHOSH',
+    text: 'Good Morning have a nice day',
+    createdAt : 123
+  });
+
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage',message);
+  });
+
   socket.on('disconnect',()=>{
     console.log('user was Disconnected');
   });
